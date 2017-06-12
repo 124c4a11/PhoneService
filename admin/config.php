b3989fdbf8041c66c0aba8c6afdf9bb7f67a1cbf
124c4a11 <124c4a11@mail.ru>
2017-06-12 20:31:23 +0300

    Initial setting opencart

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://iphoneservice/admin/');
+define('HTTP_CATALOG', 'http://iphoneservice/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://iphoneservice/admin/');
+define('HTTPS_CATALOG', 'http://iphoneservice/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/OpenServer/domains/iPhoneService/admin/');
+define('DIR_SYSTEM', 'F:/OpenServer/domains/iPhoneService/system/');
+define('DIR_IMAGE', 'F:/OpenServer/domains/iPhoneService/image/');
+define('DIR_LANGUAGE', 'F:/OpenServer/domains/iPhoneService/admin/language/');
+define('DIR_TEMPLATE', 'F:/OpenServer/domains/iPhoneService/admin/view/template/');
+define('DIR_CONFIG', 'F:/OpenServer/domains/iPhoneService/system/config/');
+define('DIR_CACHE', 'F:/OpenServer/domains/iPhoneService/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/OpenServer/domains/iPhoneService/system/storage/download/');
+define('DIR_LOGS', 'F:/OpenServer/domains/iPhoneService/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/OpenServer/domains/iPhoneService/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/OpenServer/domains/iPhoneService/system/storage/upload/');
+define('DIR_CATALOG', 'F:/OpenServer/domains/iPhoneService/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://iphoneservice/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://iphoneservice/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/OpenServer/domains/iPhoneService/catalog/');
+define('DIR_SYSTEM', 'F:/OpenServer/domains/iPhoneService/system/');
+define('DIR_IMAGE', 'F:/OpenServer/domains/iPhoneService/image/');
+define('DIR_LANGUAGE', 'F:/OpenServer/domains/iPhoneService/catalog/language/');
+define('DIR_TEMPLATE', 'F:/OpenServer/domains/iPhoneService/catalog/view/theme/');
+define('DIR_CONFIG', 'F:/OpenServer/domains/iPhoneService/system/config/');
+define('DIR_CACHE', 'F:/OpenServer/domains/iPhoneService/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/OpenServer/domains/iPhoneService/system/storage/download/');
+define('DIR_LOGS', 'F:/OpenServer/domains/iPhoneService/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/OpenServer/domains/iPhoneService/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/OpenServer/domains/iPhoneService/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
